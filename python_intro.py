a91a62fc24e16e2e7ee8df431a596c733279d0a3
Ephemeral <kim.hutchinson@me.com>
2017-06-17 13:54:25 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print('Hello there ' + name)
+hi("Kim")
+
+girls = ['Rachel', 'Monika', 'Pheobe', 'Ola',  'Kim']
+for name in girls:
+    hi(name)
+
+for i in range (1,6):
+    print(i)
